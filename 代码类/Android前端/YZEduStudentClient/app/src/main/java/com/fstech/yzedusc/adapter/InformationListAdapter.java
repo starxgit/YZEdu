eca5c57eaeb4c48fb861ce52571bb9b2af9cb8c0
starxgit <shao-xin@qq.com>
2018-03-25 17:10:49 +0800

    完成APP学生端首页静态页面

---
+++ 代码类/Android前端/YZEduStudentClient/app/src/main/java/com/fstech/yzedusc/adapter/InformationListAdapter.java
@@ -0,0 +1,85 @@
+package com.fstech.yzedusc.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.fstech.yzedusc.R;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by shaoxin on 18-3-25.
+ * 资讯列表的适配器
+ */
+
+public class InformationListAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Map<String, Object>> listItems;
+    private LayoutInflater listContainer;
+
+    public final class ViewHolder {
+        public ImageView iv_information_image;
+        public TextView tv_infomation_title;
+        public TextView getTv_infomation_date;
+    }
+
+    public InformationListAdapter(Context context, List<Map<String, Object>> listItems) {
+        this.context = context;
+        listContainer = LayoutInflater.from(context);
+        this.listItems = listItems;
+    }
+
+    @Override
+    public int getCount() {
+        return listItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder vh = null;
+        if (convertView == null) {
+            vh = new ViewHolder();
+            convertView = listContainer.inflate(R.layout.item_information, null);
+            //获得控件对象
+            vh.iv_information_image = (ImageView) convertView.findViewById(R.id.iv_information_image);
+            vh.tv_infomation_title = (TextView) convertView.findViewById(R.id.tv_information_title);
+            vh.getTv_infomation_date = (TextView) convertView.findViewById(R.id.tv_information_date);
+            //设置空间集到convertView
+            convertView.setTag(vh);
+        } else {
+            vh = (ViewHolder) convertView.getTag();
+        }
+
+        String information_image = listItems.get(position).get("information_image").toString();
+        String information_title = listItems.get(position).get("information_title").toString();
+        String information_date = listItems.get(position).get("information_date").toString();
+
+        vh.tv_infomation_title.setText(information_title);
+        vh.getTv_infomation_date.setText(information_date);
+        if(information_image.equals("0")){
+            vh.iv_information_image.setVisibility(View.GONE);
+        }else{
+            vh.iv_information_image.setVisibility(View.VISIBLE);
+        }
+
+        return convertView;
+    }
+
+}
--- 代码类/Android前端/YZEduStudentClient/app/src/main/java/com/fstech/yzedusc/fragment/MainFragment.java
+++ 代码类/Android前端/YZEduStudentClient/app/src/main/java/com/fstech/yzedusc/fragment/MainFragment.java
@@ -2,11 +2,22 @@ package com.fstech.yzedusc.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.fstech.yzedusc.R;
+import com.fstech.yzedusc.adapter.InformationListAdapter;
+import com.fstech.yzedusc.util.ListViewHeight;
+import com.oragee.banners.BannerView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by shaoxin on 18-3-25.
@@ -14,6 +25,13 @@ import com.fstech.yzedusc.R;
  */
 
 public class MainFragment extends Fragment {
+    // 定义UI对象
+    private BannerView vp_banner;
+    private int[] imgs;
+    private List<View> viewList;
+    private ListView lv_information;
+    private InformationListAdapter adapter;
+    private List<Map<String,Object>> listItems_information;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +42,69 @@ public class MainFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        initView();
+        initData();
+        setBanner();
     }
 
+    /*
+    * 初始化视图
+    * 无参数
+    * 无返回
+    * */
+    private void initView() {
+        vp_banner = (BannerView) getActivity().findViewById(R.id.vp_banner);
+        viewList = new ArrayList<View>();
+        lv_information=(ListView)getActivity().findViewById(R.id.lv_information);
+        listItems_information=new ArrayList<Map<String,Object>>();
+        adapter = new InformationListAdapter(getActivity(),listItems_information);
+        lv_information.setAdapter(adapter);
+    }
+
+    /*
+    * 初始化数据
+    * 无参数
+    * 无返回
+    * */
+    private void initData(){
+        // TODO 初始化banner 及咨询相关的数据
+        imgs= new int[]{R.drawable.makalong, R.drawable.springmvc, R.drawable.androidkaifa, R.drawable.html5};
+        for(int i=1;i<15;i++){
+            int a=(int)(Math.random()*3);
+            String img="0";
+            if(a>1) img="a";
+            Map<String, Object>listItem=new HashMap<String,Object>();
+            listItem.put("information_image",img);
+            listItem.put("information_title","咨询标题"+i);
+            listItem.put("information_date","2018-03-"+i);
+            listItems_information.add(listItem);
+        }
+        adapter.notifyDataSetChanged();
+        ListViewHeight.setListViewHeightBasedOnChildren(adapter,lv_information);
+    }
+
+    /*
+    * 配置轮播图
+    * 无参数
+    * 无返回
+    * */
+    private void setBanner(){
+        for (int i = 0; i < imgs.length; i++) {
+            ImageView image = new ImageView(getActivity());
+            final String z=i+"";
+            image.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            //设置显示格式
+            image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            image.setImageResource(imgs[i]);
+            image.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // TODO 点击banner后的事件
+                }
+            });
+            viewList.add(image);
+        }
+        vp_banner.startLoop(true);
+        vp_banner.setViewList(viewList);
+    }
 }
